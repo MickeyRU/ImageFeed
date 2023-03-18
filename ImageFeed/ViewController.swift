9bf1fb482722a0a1c4a5e5c0e700aef09a4ebf95
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-03-18 15:46:29 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Павел Афанасьев on 18.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
