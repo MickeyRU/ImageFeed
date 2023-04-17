7d4fc82dbdaa88803d51ccb4378a4c323943f113
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-04-17 15:03:08 +0300

    added new VC - LoginVC

---
+++ ImageFeed/Application/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Павел Афанасьев on 13.04.2023.
+//
+
+import UIKit
+
+let accessKey: String = "F7KZOMl0r5BGerBUC2H6WpsAbCTiIhPz-X_o55PCeGA"
+let secretKey: String = "Qao4hjYq28RSvbc-1X3WewDLLCmO1q1GTPLOFsIHRvE"
+let accessScope: String = "public+read_user+write_likes"
+let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Павел Афанасьев on 17.04.2023.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
