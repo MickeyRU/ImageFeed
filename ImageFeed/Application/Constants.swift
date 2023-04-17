1788e3b5b2e2ab37dc5f7fc343a923cf63c0ac60
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-04-17 16:40:47 +0300

    +both Auth VC ready

--- ImageFeed/Application/Constants.swift
+++ ImageFeed/Application/Constants.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-let accessKey: String = "F7KZOMl0r5BGerBUC2H6WpsAbCTiIhPz-X_o55PCeGA"
-let secretKey: String = "Qao4hjYq28RSvbc-1X3WewDLLCmO1q1GTPLOFsIHRvE"
-let accessScope: String = "public+read_user+write_likes"
-let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
-let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
+let AccessKey: String = "F7KZOMl0r5BGerBUC2H6WpsAbCTiIhPz-X_o55PCeGA"
+let SecretKey: String = "Qao4hjYq28RSvbc-1X3WewDLLCmO1q1GTPLOFsIHRvE"
+let AccessScope: String = "public+read_user+write_likes"
+let RedirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+let DefaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
