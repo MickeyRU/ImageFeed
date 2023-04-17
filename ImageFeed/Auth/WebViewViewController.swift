15f5f4817483c3aa29ea20e95be7a463a0e3cc58
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-04-17 15:51:41 +0300

    added WebView VC

---
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WebViewViewController.swift
+//  ImageFeed
+//
+//  Created by Павел Афанасьев on 17.04.2023.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewViewController: UIViewController {
+    @IBOutlet private var webView: WKWebView!
+    
+    @IBAction private func didTapBackButton(_ sender: Any?) { }
+}
