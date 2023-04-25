b511886c2283d6598b5131e6c3370477997b89fd
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-04-25 20:27:03 +0300

    small fix after codereview: - regroup constants - add weak to IBoutlet properties

--- ImageFeed/Application/Constants.swift
+++ ImageFeed/Application/Constants.swift
@@ -7,10 +7,13 @@
 
 import UIKit
 
-let AccessKey: String = "F7KZOMl0r5BGerBUC2H6WpsAbCTiIhPz-X_o55PCeGA"
-let SecretKey: String = "Qao4hjYq28RSvbc-1X3WewDLLCmO1q1GTPLOFsIHRvE"
-let AccessScope: String = "public+read_user+write_likes"
-let RedirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
-let DefaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
-let UnsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+enum Constants {
+    static let accessKey: String = "F7KZOMl0r5BGerBUC2H6WpsAbCTiIhPz-X_o55PCeGA"
+    static let secretKey: String = "Qao4hjYq28RSvbc-1X3WewDLLCmO1q1GTPLOFsIHRvE"
+    static let accessScope: String = "public+read_user+write_likes"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+}
+
 
