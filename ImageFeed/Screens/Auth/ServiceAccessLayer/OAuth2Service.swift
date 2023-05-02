98da1e9a89b41d242051239cbea6559b372ab2ac
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-05-02 16:07:20 +0300

    Race eliminated in code

--- ImageFeed/Screens/Auth/ServiceAccessLayer/OAuth2Service.swift
+++ ImageFeed/Screens/Auth/ServiceAccessLayer/OAuth2Service.swift
@@ -7,36 +7,42 @@
 
 import Foundation
 
-
 final class OAuth2Service {
     static let shared = OAuth2Service()
-
+    
     private let urlSession = URLSession.shared
-
+    
+    private var task: URLSessionTask?
+    private var lastCode: String?
+    
     private (set) var authToken: String? {
         get {
-             return OAuth2TokenStorage().token
+            return OAuth2TokenStorage().token
         }
         set {
             OAuth2TokenStorage().token = newValue
         }
     }
-
+    
     func fetchAuthToken (_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        assert(Thread.isMainThread)
+        guard lastCode != code else { return }
+        task?.cancel()
+        lastCode = code
+        
         let request = authTokenRequest(code: code)
-        print(request)
         let task = object(for: request) { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let body):
                 let authToken = body.accessToken
                 self.authToken = authToken
-                print(authToken)
                 completion(.success(authToken))
             case .failure(let error):
                 completion(.failure(error))
             }
         }
+        self.task = task
         task.resume()
     }
 }
@@ -54,16 +60,16 @@ extension OAuth2Service {
             baseURL: URL(string: "https://unsplash.com")!
         )
     }
-
+    
     private func object(for request: URLRequest, completion: @escaping (Result<OAuthTokenResponseBody, Error>) -> Void) -> URLSessionTask {
         let decoder = JSONDecoder()
         return urlSession.data(for: request) { (result: Result<Data, Error>) in
-            let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in
-                Result {
+            let response = result.flatMap { data -> Result<OAuthTokenResponseBody, Error> in Result {
                     try decoder.decode(OAuthTokenResponseBody.self, from: data)
                 }
             }
             completion(response)
+            self.task = nil
         }
     }
 }
