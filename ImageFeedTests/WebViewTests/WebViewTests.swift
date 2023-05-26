fb67506f5eb2ff328aaa10bcd276c7910f112f29
Pavel Afanasiev <afanasyevpv9@gmail.com>
2023-05-26 18:36:05 +0300

    +unittest cases "Самостоятельная задача 1"

---
+++ ImageFeedTests/WebViewTests/WebViewTests.swift
@@ -0,0 +1,41 @@
+//
+//  WebViewTests.swift
+//  WebViewTests
+//
+//  Created by Павел Афанасьев on 26.05.2023.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class WebViewTests: XCTestCase {
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled) //behaviour verification
+    }
+    
+    func testPresenterCallsLoadRequest() {
+        //given
+        let viewController = WebViewViewControllerSpy()
+        let authHelper = AuthHelper()
+        let presenter = WebViewPresenter(authHelper: authHelper)
+        viewController.presenter = presenter
+        presenter.view = viewController
+   
+        //when
+        presenter.viewDidLoad()
+        
+        //then
+        XCTAssertTrue(viewController.loadRequestCalled)
+    }
+}
